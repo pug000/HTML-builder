bff327ffdcf2435be9de7f8f14813b02c53e4a21
Roman Troshin <roman.troshin98@gmail.com>
2022-05-15 17:52:11 +0300

    feat: add 01-read-file solution

---
+++ 01-read-file/index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout } = require('process');
+const readStream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+let data = '';
+readStream.on('data', (chunk) => data = chunk);
+readStream.on('end', () => stdout.write(data));
