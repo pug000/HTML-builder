954462a24ac0e1e8726a5dffebe5dc67005a4e15
Roman Troshin <roman.troshin98@gmail.com>
2022-05-21 02:54:46 +0300

    refactor: change code 02-write-file task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -6,13 +6,13 @@ const { stdin: input, stdout: output } = process;
 const rl = readline.createInterface({ input, output });
 const writeStream = fs.createWriteStream(path.join(__dirname, 'text.txt'), 'utf-8');
 
-output.write('Write text to file:');
+output.write('Write text to file:\n');
 
 rl.on('line', (input) => {
-  input === 'exit' ? process.exit() : writeStream.write(input + '\r\n');
-})
+  input === 'exit' ? process.exit() : writeStream.write(`${input}\n`);
+});
 
 process.on('exit', () => {
   output.write('Completed text in text.txt file');
   rl.close();
-})
+});
